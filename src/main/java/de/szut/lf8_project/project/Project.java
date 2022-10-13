dad22ee5095dee72ae9103ad37fa48d21863fa5d
mkh <Malek.Hamza@dbh.de>
2022-10-13 09:16:11 +0200

    PMS-4 Erstellung der Models

---
+++ src/main/java/de/szut/lf8_project/project/Project.java
@@ -0,0 +1,41 @@
+package de.szut.lf8_project.project;
+
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Builder
+@Entity
+@Table(name = "project")
+public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotNull
+    private long responsibleEmployeeId;
+
+    @NotNull
+    private long customerId;
+
+    private String comment;
+
+    private LocalDateTime startDate;
+
+    private LocalDateTime plannedEndDate;
+
+    private LocalDateTime endDate;
+
+    @OneToMany(cascade = CascadeType.ALL,
+            fetch = FetchType.EAGER,
+            mappedBy = "project")
+    private Set<ProjectEmployee> employeeIds;
+}
---
+++ src/main/java/de/szut/lf8_project/project/ProjectEmployee.java
@@ -0,0 +1,23 @@
+package de.szut.lf8_project.project;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "project_employee")
+public class ProjectEmployee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Project project;
+
+    @NotNull
+    private long employeeId;
+}
