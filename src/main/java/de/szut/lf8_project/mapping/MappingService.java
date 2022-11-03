42ddd9296cd37ab3ebac9664e1ddaa8fd3a4c1f9
mkh <Malek.Hamza@dbh.de>
2022-11-03 09:38:40 +0100

    PMS-26 Erweiterung der Mapper Klasse um Get-Mapping
    - Erweiterung von MappingService um Get-Methoden

--- src/main/java/de/szut/lf8_project/mapping/MappingService.java
+++ src/main/java/de/szut/lf8_project/mapping/MappingService.java
@@ -1,8 +1,14 @@
 package de.szut.lf8_project.mapping;
 
+import de.szut.lf8_project.project.controllers.ProjectEmployee;
 import de.szut.lf8_project.project.dto.CreateProjectDto;
+import de.szut.lf8_project.project.dto.GetProjectDto;
 import de.szut.lf8_project.project.entities.Project;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class MappingService {
@@ -17,5 +23,29 @@ public class MappingService {
                 .startDate(dto.getStartDate())
                 .plannedEndDate(dto.getPlannedEndDate())
                 .build();
+    }
+
+    public List<GetProjectDto> mapProjectsToGetProjectDtos(List<Project> projects) {
+        return projects.stream()
+                .map(this::mapProjectToGetProjectDto)
+                .collect(Collectors.toList());
+    }
+
+    public GetProjectDto mapProjectToGetProjectDto(Project project) {
+        GetProjectDto dto = new GetProjectDto();
+        dto.setId(project.getId());
+        dto.setComment(project.getComment());
+        dto.setCustomerId(project.getCustomerId());
+        dto.setResponsibleEmployeeId(project.getResponsibleEmployeeId());
+        dto.setStartDate(project.getStartDate());
+        dto.setPlannedEndDate(project.getPlannedEndDate());
+        dto.setEndDate(project.getEndDate());
+
+        Set<Long> employeeIds = project.getEmployeeIds().stream()
+                .map(ProjectEmployee::getEmployeeId)
+                .collect(Collectors.toSet());
+        dto.setEmployeeIds(employeeIds);
+
+        return dto;
     }
 }
