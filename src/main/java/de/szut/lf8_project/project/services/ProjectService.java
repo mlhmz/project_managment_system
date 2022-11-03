7a6bc73990184f7323c38d1dbf5be35cf9228672
mul <moritz.ulrich.hb@googlemail.com>
2022-11-03 10:26:35 +0100

    PMS-15 Erweiterung des Projekt-Services um eine update-Methode - updateProject Methode eingefügt, welche ChangeProjectDto annimmt und geänderte Daten in das Projekt der angegeben ID schreibt

--- src/main/java/de/szut/lf8_project/project/services/ProjectService.java
+++ src/main/java/de/szut/lf8_project/project/services/ProjectService.java
@@ -1,8 +1,12 @@
 package de.szut.lf8_project.project.services;
 
+import de.szut.lf8_project.exceptionHandling.ResourceNotFoundException;
+import de.szut.lf8_project.project.dto.ChangeProjectDto;
 import de.szut.lf8_project.project.repositories.ProjectRepository;
 import de.szut.lf8_project.project.entities.Project;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class ProjectService {
@@ -16,5 +20,26 @@ public class ProjectService {
         return this.repository.save(project);
     }
 
+    public Project updateProject(ChangeProjectDto dto, Long id){
+        Optional<Project> optProject = this.repository.findById(id);
 
+        if(optProject.isPresent()){
+            return updateProjectData(dto, optProject.get());
+        } else{
+            throw new ResourceNotFoundException("No Project was found for id" + id);
+        }
+    }
+
+    private Project updateProjectData(ChangeProjectDto dto, Project project) {
+        if(!dto.getComment().isEmpty()){
+            project.setComment(dto.getComment());
+        }
+        if(dto.getPlannedEndDate() != null){
+            project.setPlannedEndDate(dto.getPlannedEndDate());
+        }
+        if(dto.getResponsibleEmployeeId() != null){
+            project.setResponsibleEmployeeId(dto.getResponsibleEmployeeId());
+        }
+        return this.repository.save(project);
+    }
 }
