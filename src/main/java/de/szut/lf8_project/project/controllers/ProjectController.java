1ff5409833c968f8d93add5a3fd8eecc2d767711
mul <moritz.ulrich.hb@googlemail.com>
2022-11-09 10:20:58 +0100

    PMS-36 Erweiterung des Projekt-Services um eine DELETE-Rest-Schnittstelle zum Löschen eines Mitarbeiters - deleteEmployeeFromProject DELETE-Rest-Schnittstelle eingefügt

--- src/main/java/de/szut/lf8_project/project/controllers/ProjectController.java
+++ src/main/java/de/szut/lf8_project/project/controllers/ProjectController.java
@@ -69,4 +69,40 @@ public class ProjectController {
                 mappingService.mapCreateProjectDtoToProject(dto)
         ), HttpStatus.CREATED);
     }
+
+    /**
+     * Löscht einen Mitarbeiter aus einem Project mittels Delete-Request
+     * <p>
+     * TODO: Sobald das GetObjectDto erstellt wurde sollte dies durch das Project als ResponseEntity ersetzt werden
+     */
+    @Operation(summary = "creates a new project with its id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "created project",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = CreateProjectDto.class))}),
+            @ApiResponse(responseCode = "400", description = "If employee is not involved in project",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "If the project or the employee could't be found")}
+    )
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Project> deleteEmployeeFromProject(@PathVariable Long id, @RequestBody long employeeId,
+                                                             @RequestHeader(HttpHeaders.AUTHORIZATION) String token){
+        if (employeeService.isEmployeeExisting(employeeId, token)) {
+            // TODO: Nötigen HATEOAS setzen
+            if(this.projectService.isEmployeeInvolvedInProject(id,employeeId)){
+                if(this.projectService.deleteEmployeeFromProject(id,employeeId)){
+                    return new ResponseEntity<>(HttpStatus.OK);
+                }else{
+                    throw new RuntimeException(String.format("Something went wrong in the deletion process for " +
+                                    "project %d and employee %d", id, employeeId));
+                }
+            }else{
+                throw new ResourceNotFoundException(String.format("The employee %d couldn't be found in project %d.",
+                        employeeId, id));
+            }
+        } else {
+            throw new ResourceNotFoundException(String.format("The employee with the id %d couldn't be found.",
+                    employeeId));
+        }
+    }
 }
