741add438ccc40180d250b51f59cebffe4aab359
mul <moritz.ulrich.hb@googlemail.com>
2022-11-09 10:22:27 +0100

    PMS-35 Erweiterung des Projekt-Services um eine delete-Methode zum Löschen eines Mitarbeiters - deleteEmployeeFromProject Methode in ProjectService eingefügt - ProjectEmployeeRepository angelegt

---
+++ src/main/java/de/szut/lf8_project/project/repositories/ProjectEmployeeRepository.java
@@ -0,0 +1,11 @@
+package de.szut.lf8_project.project.repositories;
+
+import de.szut.lf8_project.project.controllers.ProjectEmployee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ProjectEmployeeRepository extends JpaRepository<ProjectEmployee, Long> {
+    Optional<ProjectEmployee> findProjectEmployeeByProjectIdAndEmployeeId(long projectId, long employeeId);
+    boolean deleteProjectEmployeeByProjectIdAndEmployeeId(long projectId, long employeeId);
+}
--- src/main/java/de/szut/lf8_project/project/services/ProjectService.java
+++ src/main/java/de/szut/lf8_project/project/services/ProjectService.java
@@ -1,20 +1,42 @@
 package de.szut.lf8_project.project.services;
 
+import de.szut.lf8_project.exceptionHandling.ResourceNotFoundException;
+import de.szut.lf8_project.project.controllers.ProjectEmployee;
+import de.szut.lf8_project.project.repositories.ProjectEmployeeRepository;
 import de.szut.lf8_project.project.repositories.ProjectRepository;
 import de.szut.lf8_project.project.entities.Project;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class ProjectService {
     private final ProjectRepository repository;
+    private final ProjectEmployeeRepository projectEmployeeRepository;
 
-    public ProjectService(ProjectRepository repository) {
+    public ProjectService(ProjectRepository repository, ProjectEmployeeRepository projectEmployeeRepository) {
         this.repository = repository;
+        this.projectEmployeeRepository = projectEmployeeRepository;
     }
 
     public Project createProject(Project project) {
         return this.repository.save(project);
     }
 
+    public boolean isEmployeeInvolvedInProject(long projectId, long employeeId){
+        Optional<Project> oProject = this.repository.findById(projectId);
+        if(oProject.isPresent()){
+            Optional<ProjectEmployee> oEmployee =
+                    this.projectEmployeeRepository.findProjectEmployeeByProjectIdAndEmployeeId(projectId, employeeId);
+                return oEmployee.isPresent();
+        } else {
+            // TODO: 400 Bad Request wenn employee nicht in Project beteiligt ist.
+            throw new ResourceNotFoundException(String.format("The project with the id %d couldn't be found.",
+                    projectId));
+        }
+    }
 
+    public boolean deleteEmployeeFromProject(long projectId, long employeeId){
+        return this.projectEmployeeRepository.deleteProjectEmployeeByProjectIdAndEmployeeId(projectId,employeeId);
+    }
 }
