a1de204dbcf589e3cc34cea01afdf6cde8d080e4
Malek Hamza <malek.hamza@gmx.de>
2022-11-14 23:23:09 +0100

    PMS-11: Docs ergänzt

--- src/main/java/de/szut/lf8_project/mapping/MappingService.java
+++ src/main/java/de/szut/lf8_project/mapping/MappingService.java
@@ -32,12 +32,24 @@ public class MappingService {
                 .build();
     }
 
+    /**
+     * Maps a List of Project to a list of {@link GetProjectDto}
+     *
+     * @param projects object to map
+     * @return {@link List} of {@link GetProjectDto}
+     */
     public List<GetProjectDto> mapProjectsToGetProjectList(List<Project> projects) {
         return projects.stream()
                 .map(this::mapProjectToGetProjectDto)
                 .collect(Collectors.toList());
     }
 
+    /**
+     * Maps a Project to GetProjectDto
+     *
+     * @param project Data to fill into the {@link GetProjectDto}
+     * @return GetProjectDto with filled Data
+     */
     public GetProjectDto mapProjectToGetProjectDto(Project project) {
         GetProjectDto dto = new GetProjectDto();
         dto.setId(project.getId());
@@ -55,6 +67,12 @@ public class MappingService {
         return dto;
     }
 
+    /**
+     * Maps ProjectEmployee to a Set of {@link EmployeeReferenceDto}
+     *
+     * @param projectEmployees set of objects to map into references
+     * @return HATEOAS CollectionModel with EmployeeReferences
+     */
     public CollectionModel<EmployeeReferenceDto> mapProjectEmployeesToEmployeeReferences(Set<ProjectEmployee> projectEmployees) {
         if (projectEmployees != null && !projectEmployees.isEmpty()) {
             return this.employeeReferenceModelAssembler.toCollectionModel(projectEmployees);
@@ -63,17 +81,35 @@ public class MappingService {
         }
     }
 
+    /**
+     * Maps a Set of {@link ProjectEmployee} to {@link GetEmployeeReferencesDto}.
+     *
+     * @param projectEmployees objects to map
+     * @return GetEmployeeReferencesDto filled with ProjectEmployee's
+     */
     public GetEmployeeReferencesDto mapProjectEmployeesToGetEmployeeReferencesDto(Set<ProjectEmployee> projectEmployees) {
         return new GetEmployeeReferencesDto(mapProjectEmployeesToEmployeeReferences(projectEmployees));
     }
 
+    /**
+     * Maps {@link ProjectEmployee} to an EmployeeReferenceDto
+     *
+     * @param employee object to map
+     * @return Dto filled with ProjectEmployee
+     */
     public EmployeeReferenceDto mapProjectEmployeeToEmployeeReference(ProjectEmployee employee) {
         return this.employeeReferenceModelAssembler.toModel(employee);
     }
 
-    private ProjectEmployee buildProjectEmployeeById(@NotNull long responsibleEmployeeId) {
+    /**
+     * Builds a ProjectEmployee with an Employee ID
+     *
+     * @param employeeId Employee ID to put into the ProjectEmployee
+     * @return ProjectEmployee with ID
+     */
+    private ProjectEmployee buildProjectEmployeeById(@NotNull long employeeId) {
         ProjectEmployee projectEmployee = new ProjectEmployee();
-        projectEmployee.setEmployeeId(responsibleEmployeeId);
+        projectEmployee.setEmployeeId(employeeId);
         return projectEmployee;
     }
 }
