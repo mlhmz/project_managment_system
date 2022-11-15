0a609eaec43743a3c8cfd2b77f86cb87e53b943b
Malek Hamza <malek.hamza@gmx.de>
2022-11-15 23:27:50 +0100

    PMS-14: Removed unused Repo Method

--- src/main/java/de/szut/lf8_project/project/repositories/ProjectEmployeeRepository.java
+++ src/main/java/de/szut/lf8_project/project/repositories/ProjectEmployeeRepository.java
@@ -3,10 +3,7 @@ package de.szut.lf8_project.project.repositories;
 import de.szut.lf8_project.project.entities.ProjectEmployee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface ProjectEmployeeRepository extends JpaRepository<ProjectEmployee, Long> {
-    Optional<ProjectEmployee> findProjectEmployeeByProjectIdAndEmployeeId(long projectId, long employeeId);
     boolean existsProjectEmployeeByProjectIdAndEmployeeId(long projectId, long employeeId);
     boolean deleteProjectEmployeeByProjectIdAndEmployeeId(long projectId, long employeeId);
 }
