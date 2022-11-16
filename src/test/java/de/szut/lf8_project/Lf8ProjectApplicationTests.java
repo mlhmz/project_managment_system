b0b6dcbf497a64e2a93029e1f11cd1acf2b1646f
mkh <malek.hamza@dbh.de>
2022-11-16 16:45:11 +0100

    Exclusion von Default Test Class

--- src/test/java/de/szut/lf8_project/Lf8ProjectApplicationTests.java
+++ src/test/java/de/szut/lf8_project/Lf8ProjectApplicationTests.java
@@ -2,8 +2,10 @@ package de.szut.lf8_project;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootTest
+@Profile("localtests")
 class Lf8ProjectApplicationTests {
 
     @Test
