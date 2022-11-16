d52b241bc3fd478858a1d0af30e633d368c07285
mkh <malek.hamza@dbh.de>
2022-11-16 09:00:57 +0100

    PMS-42: Repo aus Service entfernt

--- src/main/java/de/szut/lf8_project/customer/CustomerService.java
+++ src/main/java/de/szut/lf8_project/customer/CustomerService.java
@@ -4,9 +4,6 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CustomerService {
-    private final CustomerRepository repository;
-
-    public CustomerService(CustomerRepository repository){this.repository = repository;}
 
     public boolean checkIfCustomerExists(Long customerId){
         return true;
