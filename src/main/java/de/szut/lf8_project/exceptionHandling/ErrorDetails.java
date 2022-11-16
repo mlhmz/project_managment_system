1e6285a5c385337fceeee9aff07ed4a57373b2a5
mkh <malek.hamza@dbh.de>
2022-11-16 18:19:44 +0100

    PMS-41 Fachliche Exceptions werden lediglich als Bad Request oder Internal Server Error angezeigt

--- src/main/java/de/szut/lf8_project/exceptionHandling/ErrorDetails.java
+++ src/main/java/de/szut/lf8_project/exceptionHandling/ErrorDetails.java
@@ -1,12 +1,14 @@
 package de.szut.lf8_project.exceptionHandling;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
+@Builder
 public class ErrorDetails {
     private LocalDateTime timestamp;
     private String message;
--- src/main/java/de/szut/lf8_project/exceptionHandling/GlobalExceptionHandler.java
+++ src/main/java/de/szut/lf8_project/exceptionHandling/GlobalExceptionHandler.java
@@ -2,11 +2,15 @@ package de.szut.lf8_project.exceptionHandling;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 
 import java.time.LocalDateTime;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -21,6 +25,37 @@ public class GlobalExceptionHandler {
     public ResponseEntity<?> handleUnprocessableEntityException(UnprocessableEntityException ex, WebRequest request) {
         ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(), ex.getMessage(), request.getDescription(false));
         return new ResponseEntity<>(errorDetails, HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseBody
+    public ResponseEntity<ErrorDetails> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex,
+                                                                              WebRequest request) {
+        String errorMessage = ex.getBindingResult().getAllErrors()
+                .stream()
+                .map(oe -> oe.getCode() + ": " + oe.getDefaultMessage())
+                .collect(Collectors.joining("; "));
+
+        ErrorDetails errorDetails = ErrorDetails.builder()
+                .timestamp(LocalDateTime.now())
+                .message(errorMessage)
+                .details(request.getDescription(false))
+                .build();
+
+        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseBody
+    public ResponseEntity<ErrorDetails> handleAnyException(Exception ex, WebRequest request) {
+        ErrorDetails errorDetails = ErrorDetails.builder()
+                .timestamp(LocalDateTime.now())
+                .message(ex.getMessage())
+                .details(request.getDescription(false))
+                .build();
+
+        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
 }
