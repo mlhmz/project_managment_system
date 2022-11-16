f957165197c75417e5744e0c13b1c0100c6caf08
mkh <malek.hamza@dbh.de>
2022-11-16 09:02:10 +0100

    PMS-42: Docs ergänzt

--- src/main/java/de/szut/lf8_project/customer/CustomerService.java
+++ src/main/java/de/szut/lf8_project/customer/CustomerService.java
@@ -4,8 +4,13 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CustomerService {
-
-    public boolean checkIfCustomerExists(Long customerId){
+    /**
+     * Checks if Customer exists
+     *
+     * @param customerId The actual id of the customer
+     * @return boolean if customer exists
+     */
+    public boolean checkIfCustomerExists(Long customerId) {
         return true;
     }
 }
