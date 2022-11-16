574eca9499a5251ae0e90ef7bf961c5c368da472
mkh <malek.hamza@dbh.de>
2022-11-16 16:57:23 +0100

    Fixed Test Config

--- src/test/java/de/szut/lf8_project/Lf8ProjectApplicationTests.java
+++ src/test/java/de/szut/lf8_project/Lf8ProjectApplicationTests.java
@@ -2,10 +2,8 @@ package de.szut.lf8_project;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Profile;
 
 @SpringBootTest
-@Profile("localtests")
 class Lf8ProjectApplicationTests {
 
     @Test
