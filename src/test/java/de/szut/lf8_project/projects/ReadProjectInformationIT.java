02c034ee70dcd66848b700cd20b33f588e51c125
mkh <malek.hamza@dbh.de>
2022-11-16 15:19:43 +0100

    PMS-40 Added readAllProjects() to IT

--- src/test/java/de/szut/lf8_project/projects/ReadProjectInformationIT.java
+++ src/test/java/de/szut/lf8_project/projects/ReadProjectInformationIT.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpHeaders;
 
 import java.time.LocalDateTime;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -33,5 +34,28 @@ public class ReadProjectInformationIT extends AuthorizedIT {
                 .andExpect(jsonPath("comment", is("This is a new project!")))
                 .andExpect(jsonPath("startDate", is("2022-12-24T12:00:00")))
                 .andExpect(jsonPath("plannedEndDate", is("2022-12-27T23:59:59")));
+    }
+
+    @Test
+    public void readAllProjects() throws Exception {
+        for (int i = 0; i < 3; i++) {
+            Project project = Project.builder()
+                    .responsibleEmployeeId(9L + i)
+                    .customerId(1L + i)
+                    .startDate(LocalDateTime.of(2022, 12, 24, 12, 0, 0))
+                    .plannedEndDate(LocalDateTime.of(2022, 12, 27, 23, 59, 59))
+                    .comment("This is a new project!")
+                    .build();
+
+            projectRepository.save(project);
+        }
+
+        this.mockMvc.perform(get("/project")
+                .header(HttpHeaders.AUTHORIZATION, fetchJWT()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(3)))
+                .andExpect(jsonPath("$[0].id", is(1)))
+                .andExpect(jsonPath("$[1].id", is(2)))
+                .andExpect(jsonPath("$[2].id", is(3)));
     }
 }
